da048f4e3541a85f1ec43cbd6543605bc8a9dab9
phamhoan191 <phamhoan191@gmail.com>
2020-07-16 18:56:56 +0700

    Create app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <ul>
+      <li>Go to market</li>
+      <li>Buy food</li>
+      <li>Make dinner</li>
+    </ul>
+  );
+}
+
+export default App;
